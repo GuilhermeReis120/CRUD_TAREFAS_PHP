dba61f2110f3a939bafbbf2e0e43df2c3630de8b
GuilhermeReis120 <souzaguireis@gmail.com>
2025-11-21 18:26:08 -0300

    feito alterações nos models, função listar, criar, atualizar e apagar.

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,7 @@
 const API_URL = "http://localhost:8000";
 
 const form = document.querySelector('form');
+const tarefas = [];
 
 function adicionarTarefa(event) {
   event.preventDefault();
@@ -8,6 +9,13 @@ function adicionarTarefa(event) {
   const nome_tarefaInput = document.getElementById("nome_tarefa");
   const descricao_tarefaInput = document.getElementById("descricao_tarefa");
 
-  console.log('Adicionar Tarefa:', nome_tarefaInput.value, descricao_tarefaInput.value);
+  console.log('Adicionar Tarefa:', nome_tarefaInput.value);
+  console.log('Descrição Tarefa:', descricao_tarefaInput.value);
+
+  tarefas.push({
+    nome: nome_tarefaInput.value,
+    descricao: descricao_tarefaInput.value
+  })
+  console.log('Tarefas atuais:', tarefas);
 }
-  form.addEventListener('submit', adicionarTarefa);
+form.addEventListener('submit', adicionarTarefa);
